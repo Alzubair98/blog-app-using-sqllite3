c835e939fcd8e675b30519be88a2621258d789ca
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-19 01:01:23 +0300

    add show article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,6 @@
+class ArticlesController < ApplicationController
+   
+    def show
+      @article = Article.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :articles, only: [:show]
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  
+  #this will give us all the routes for articles
+  
 end
