d08b2f9f175cbeefe02bf1b6d3a296bc7da19464
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-20 18:29:26 +0300

    add edit article funtion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
       @article = Article.new
     end
 
+    def edit 
+      @article = Article.find(params[:id])
+    end
+
     def create
       @article = Article.new(params.require(:article).permit(:title, :description))
       if @article.save
@@ -18,6 +22,16 @@ class ArticlesController < ApplicationController
         redirect_to @article 
       else
         render 'new'
+      end
+    end
+
+    def update 
+      @article = Article.find(params[:id])
+      if @article.update(params.require(:article).permit(:title, :description))
+          flash[:notice] = "Article was updated"
+          redirect_to @article
+      else 
+        render 'edit'
       end
     end
 
