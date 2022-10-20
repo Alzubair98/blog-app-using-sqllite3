c7f1a2a07ebdf43c039a637910f252d2bc137921
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-20 19:52:25 +0300

    add delete function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index,:new, :create, :edit, :update]
+  resources :articles #this will give us all the routes for articles
   root 'pages#home'
   get 'about', to: 'pages#about'
   
-  #this will give us all the routes for articles
+  
   
 end
