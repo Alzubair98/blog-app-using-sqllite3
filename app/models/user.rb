d6b44c8afec8fccc9ade15cf62472b2423ab4e0e
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-29 18:26:00 +0300

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-    
+    validates :username, presence: true, length: { maximum: 25 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email,
+    presence: true, length: { maximum: 105 }, uniqueness: true,
+    format: { with: VALID_EMAIL_REGEX}
+
 end
