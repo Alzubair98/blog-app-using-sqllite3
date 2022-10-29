27d0c004af5b3534d8a6e93ff802b8ea9d46c175
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-29 18:54:22 +0300

    add user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :articles
     validates :username, presence: true, length: { maximum: 25 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email,
