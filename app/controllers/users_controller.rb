26fa93ac8b09732d9f84b7214ad9fa27bf7fb47f
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-30 20:43:19 +0300

    create new user from browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,20 @@ class UsersController < ApplicationController
     end
 
     def create 
-      @user = User.new()
+      @user = User.new(user_params)
+      if @user.save 
+        flash[:notice] = "Welcome to the blog #{@user.username.capitalize} , you have successfully signed up"
+        redirect_to articles_path
+      else  
+        render 'new'
+      end
+    end
+
+
+
+    private
+    def user_params
+      params.require(:user).permit(:username, :email, :password_digest)
     end
   end
   
