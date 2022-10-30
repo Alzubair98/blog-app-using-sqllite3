afeed8a33b70dd8151d03e9b23091e7374563034
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-30 14:22:15 +0300

    change the email to lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
     validates :username, presence: true, length: { maximum: 25 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email,
-    presence: true, length: { maximum: 105 }, uniqueness: true,
+    presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false },
     format: { with: VALID_EMAIL_REGEX}
 
 end
