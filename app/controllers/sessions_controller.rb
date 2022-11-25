c0f5bd4982290ca8e678546bdcad48cdd9e53cf4
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-25 19:30:47 +0300

    fix login bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
-            falsh[:notice] = "Logged in successfully"
+            flash[:notice] = "Logged in successfully"
             redirect_to user
 
         else  
