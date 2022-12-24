9c31097174354a76ca741f3a6fda081d7691f2e3
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-12-24 19:24:22 +0300

    fix logout button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,8 @@ class SessionsController < ApplicationController
             flash[:notice] = "Logged in successfully"
             redirect_to user
         else  
-            flash.now[:alert]= "There was something wrong with your login details"
-            render 'new'
+            flash[:alert] = "There was something wrong with your login details"
+            redirect_to "/login"
         end
     end
 
