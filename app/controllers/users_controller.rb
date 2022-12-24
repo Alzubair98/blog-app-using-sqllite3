957b39ea487bf8fcfcb874065ead5fe993380bad
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-12-24 19:51:14 +0300

    fix sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # we can use before_action method just like that 
+  # before_action :set_user, only: [:show, :edit, :update]
+  
     def show 
       set_user
       @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -16,6 +19,7 @@ class UsersController < ApplicationController
     def create 
       @user = User.new(user_params)
       if @user.save 
+        session[:user_id] = @user.id
         flash[:notice] = "Welcome to the blog #{@user.username.capitalize} , you have successfully signed up"
         redirect_to articles_path
       else  
@@ -42,6 +46,7 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
+
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
         gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
         image_tag(gravatar_url, alt: user.username, class: "rounded shadow-lg mx-auto d-block mt-4")
     end
+
+    def current_user 
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user  #(!! is to turn the object to a boolean)
+    end
 end
