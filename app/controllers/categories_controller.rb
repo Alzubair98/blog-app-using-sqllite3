2cb43f581c63224495a3804000a444b6d8546a4c
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-12-28 21:47:17 +0300

    create a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,28 @@
 class CategoriesController < ApplicationController
 
     def new 
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save 
+            flash[:notice] ="Category was Created successfully"
+            redirect_to @category
+        else 
+            render 'new'
+        end
     end
 
     def index 
     end 
 
     def show 
+    end
+
+    private 
+    def category_params 
+        params.require(:category).permit(:name)
     end
 
 end
