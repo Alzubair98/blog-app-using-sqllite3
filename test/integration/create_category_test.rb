3d37fdedcfe5d99437bcf9afcaf482abb672fd08
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-12-28 23:15:12 +0300

    error test

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -12,4 +12,18 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body
   end
+
+  #test for error msg
+
+  # test "get new category form and regect invalid category submission" do
+  #   get "/categories/new"
+  #   assert_response :success 
+  #   assert_no_difference "Category.count" do 
+  #     post categories_path, params:{ category: {name: ""}}
+      
+  #   end
+  #   assert_match "errors", response.body
+  #   assert_select 'div.alert'
+  #   assert_select 'h4.alert-heading'
+  # end
 end
