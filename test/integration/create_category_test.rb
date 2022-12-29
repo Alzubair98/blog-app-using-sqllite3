642b129490079b433ac8b68af5dd5c60aff2eaaf
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-12-30 02:45:55 +0300

    fix bugs

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username: 'alzubair', email: 'zubair@gmail.com', password:'password',
+      admin: true )
+      sign_in_as(@admin_user)
+  end
   test "get new category form and create category" do
     get "/categories/new"
     assert_response :success 
