d9e958b31cf06726b4c335c1a505afd438b5e708
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-12-31 16:00:28 +0300

    add many-to-many relationship to articles & categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_190028) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_31_123428) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
